25150e4a56cdaa0dcedec923c4ca5aa2045796a0
calebfj <cfjohnson23@gmail.com>
2024-11-02 21:05:14 -0500

    we gaming

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,6 +4,15 @@ extends CharacterBody2D
 @export var gravity := 980
 @export var jump_force := -300
 @export var sprint_multiplier := 2
+var left_pressed = false
+var right_pressed = false
+var last_direction_pressed = 0
+
+@export var wall_slide_speed := 100
+@export var wall_jump_force := Vector2(300, -400)
+@export var max_wall_jumps := 1
+var wall_jumps_remaining := max_wall_jumps
+var is_wall_sliding := false
 
 @export var coyote_time_duration := 0.125
 var coyote_time_remaining := 0.0
@@ -12,45 +21,80 @@ var coyote_time_remaining := 0.0
 
 func _ready():
 	animated_sprite.play("idle")
+	
+func _input(event):
+	if event.is_action_pressed("move_left"):
+		left_pressed = true
+		last_direction_pressed = -1
+	if event.is_action_pressed("move_right"):
+		right_pressed = true
+		last_direction_pressed = 1
+	if event.is_action_released("move_left"):
+		left_pressed = false
+		if right_pressed:
+			last_direction_pressed = 1
+		else:
+			last_direction_pressed = 0
+	if event.is_action_released("move_right"):
+		right_pressed = false
+		if left_pressed:
+			last_direction_pressed = -1
+		else:
+			last_direction_pressed = 0
 
 func _physics_process(delta):
 	var is_grounded := is_on_floor()
+	var is_touching_wall := is_on_wall()
 	
 	if is_grounded:
 		coyote_time_remaining = coyote_time_duration
+		wall_jumps_remaining = max_wall_jumps
 	else:
 		coyote_time_remaining -= delta
 	
-	if Input.is_action_just_pressed("move_up") and coyote_time_remaining > 0.0:
-		velocity.y = jump_force
-		coyote_time_remaining = 0.0
-	
-	if not is_grounded:
-		velocity.y += gravity * delta
-	
-	var direction := 0
+	var direction = last_direction_pressed
 	
 	var sprint := Input.is_action_pressed("move_fast")
-	var right := Input.is_action_pressed("move_right")
-	var left := Input.is_action_pressed("move_left")
+	var jump := Input.is_action_pressed("move_up")
 	
-	if right and not left:
-		direction = 1
-	elif left and not right:
-		direction = -1
-		
 	var current_speed := speed
 	if sprint and direction != 0:
 		current_speed *= sprint_multiplier
 	
 	velocity.x = direction * current_speed
 	
+	is_wall_sliding = false
+	if is_touching_wall and not is_grounded and velocity.y > 0:
+		is_wall_sliding = true
+		velocity.y = min(velocity.y, wall_slide_speed)
+	
+	if jump:
+		if coyote_time_remaining > 0.0:
+			velocity.y = jump_force
+			coyote_time_remaining = 0.0
+		elif is_wall_sliding and wall_jumps_remaining > 0:
+			velocity.y = wall_jump_force.y
+			velocity.x = wall_jumps_remaining * get_wall_jump_direction()
+			wall_jumps_remaining -= 1
+			is_wall_sliding = false
+	
 	if direction != 0:
 		animated_sprite.play("walk")
 		animated_sprite.flip_h = direction > 0
 	else:
 		animated_sprite.play("idle")
-
+	
+	if not is_grounded and not is_wall_sliding:
+		velocity.y += gravity * delta
 	
 	move_and_slide()
 	
+func get_wall_jump_direction() -> int:
+	if is_on_wall():
+		var collision = get_last_slide_collision()
+		if collision:
+			if collision.get_normal().x > 0:
+				return -1
+			elif collision.get_normal().x < 0:
+				return 1
+	return 0
