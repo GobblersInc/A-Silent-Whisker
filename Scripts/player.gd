72619a6564f2e2b508824c09c6320deffd668217
jaidenbm0127 <64822674+jaidenbm0127@users.noreply.github.com>
2024-11-03 19:42:04 -0500

    Added walk, juimp animation
    Also tweaked idle/default model

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
 @export var speed := 100
-@export var sneak_speed := 45
 @export var gravity := 980
 @export var jump_force := -300
 @export var sprint_multiplier := 2
@@ -15,7 +14,7 @@ var last_direction_pressed = 0
 var wall_jumps_remaining := max_wall_jumps
 var is_wall_sliding := false
 
-@export var coyote_time_duration := 0.123
+@export var coyote_time_duration := 0.125
 var coyote_time_remaining := 0.0
 
 @onready var animated_sprite := $AnimatedSprite2D
@@ -56,17 +55,14 @@ func _physics_process(delta):
 	var direction = last_direction_pressed
 	
 	var sprint := Input.is_action_pressed("move_fast")
-	var jump := Input.is_action_just_pressed("move_up")
-	var sneak := Input.is_action_pressed("move_down")
+	var jump := Input.is_action_pressed("move_up")
 	
 	var current_speed := speed
 	if sprint and direction != 0:
 		current_speed *= sprint_multiplier
-		
-	if sneak and direction != 0:
-		current_speed = sneak_speed
 	
-	# WALL SLIDING LOGIC + ANIMATION TRIGGER
+	velocity.x = direction * current_speed
+	
 	is_wall_sliding = false
 	if is_touching_wall and not is_grounded and velocity.y > 0:
 		is_wall_sliding = true
@@ -82,36 +78,24 @@ func _physics_process(delta):
 	else:
 		animated_sprite.rotation_degrees = 0
 	
-	# JUMP LOGIC (GROUND AND WALL + COYOTE TIME)
 	if jump:
 		if coyote_time_remaining > 0.0:
 			velocity.y = jump_force
 			coyote_time_remaining = 0.0
 		elif is_wall_sliding and wall_jumps_remaining > 0:
 			velocity.y = wall_jump_force.y
-			direction = get_wall_jump_direction()
+			velocity.x = wall_jumps_remaining * get_wall_jump_direction()
 			wall_jumps_remaining -= 1
 			is_wall_sliding = false
 	
-	# APPLYING ANIMATION 
 	if direction != 0:
-		if sneak:
-			animated_sprite.play("sneak")
-		else:
-			animated_sprite.play("walk")
+		animated_sprite.play("walk")
 		animated_sprite.flip_h = direction > 0
 	else:
-		if sneak:
-			animated_sprite.play("sneak")
-		else:
-			animated_sprite.play("idle")
+		animated_sprite.play("idle")
 	
-	# GRAVITY CALCULATION
 	if not is_grounded and not is_wall_sliding:
 		velocity.y += gravity * delta
-	
-	# APPLIES THE MOVEMENT VELOCITY
-	velocity.x = direction * current_speed
 	
 	move_and_slide()
 	
