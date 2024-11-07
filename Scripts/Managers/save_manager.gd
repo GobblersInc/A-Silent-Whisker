9e28b0ed173dca9bc3e97b2635c5d236f68c0c98
Tallon <tallon.mcabee@gmail.com>
2024-11-07 16:12:08 -0600

    Created save_manager

---
+++ Scripts/Managers/save_manager.gd
@@ -0,0 +1,11 @@
+extends Node
+
+# Add save abilities for character position so you can revert back to their previous place in the scene or location
+# Serializing dictionary to file
+# Binary Serialization (Appraoch to take)
+# https://docs.godotengine.org/en/stable/tutorials/io/saving_games.html
+
+class_name Save
+
+func _init():
+	print("MY GOD A NEW SAVEEE!!!")
