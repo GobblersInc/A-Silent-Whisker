5eb1bb1079dbafaec4409bb5c9901ab88f4655ab
Tallon <tallon.mcabee@gmail.com>
2024-11-10 02:41:05 -0600

    Added Save class and Manager
    Need to test and implement into game flow (Utilize within pause scripts more then likely)

--- Scripts/Managers/save_manager.gd
+++ Scripts/Managers/save_manager.gd
@@ -5,15 +5,34 @@ extends Node
 func _ready():
 	pass # Replace with function body.
 
-func save_game():
+func save_game(save_name: String):
 	# Need try catch to make sure there are no repeating save names
-	var new_name = "{randomid}".format({"randomid": str(randi_range(0, 1000))})
-	var nodes = _Player.get_property_list()
-	var new_path = "thisisatestpath"
+	# var nodes = _Player.get_property_list()
+	# var tree_nodes = get_tree().get_nodes_in_group("Persist")
+	var new_path = "res://User/Nodes/"
+	var player_x = _Player.postion.x
+	var player_y = _Player.postion.y
 	
-	var new_save = Save.new(new_name, new_path, nodes)
+	print("Player position is: {nx}, {ny}".format({"nx": player_x, "ny": player_y}))
 	
-	print("Game is being saved!")
+	var player_data = {
+		"x": player_x,
+		"y": player_y,
+	}
+	
+	var new_save = Save.new(save_name, new_path, player_data)
+	new_save.save_to_file()
+	
+	print("Game has been saved!")
 
-func load_game():
-	print("Loading previous game!")
+func load_game(load_name: String):
+	var temp_object = Save.new(load_name)
+	var player_data = temp_object.load_from_file()
+	if player_data == OK:
+		_Player.position.x = player_data["x"]
+		_Player.position.y = player_data["y"]
+		print("Loaded previous game!")
+	else:
+		print("Failed to load previous game!")
+	
+	
--- Scripts/Objects/save.gd
+++ Scripts/Objects/save.gd
@@ -8,17 +8,51 @@ extends Node
 # Use PackedScenes to store nodes and their properties
 # Use Resources to save player specific data (Score, lives, etc, etc), settings, etc
 
+# Save player position and current scene (All we need right now)
+# Can add delivery saves later
+
+# Transition parallax backgrounds (500 x 200)
+
 class_name Save
 
 var save_name: String
+var complete_path: String
 var path: String
-var nodes: Object
+var nodes: Variant
 
-func _init(init_name: String, init_path: String, init_nodes: Object):
+func _init(init_name: String = "default", init_path: String = "res://User/Nodes/", init_nodes: Variant = ""):
 	self.save_name = init_name
 	self.path = init_path
 	self.nodes = init_nodes
+	self.complete_path = self.path + "{save_name}.cfg".format({"save_name": self.save_name})
+	
+func save_to_file() -> void:
+	var save_file = FileAccess.open(self.complete_path, FileAccess.WRITE)
+	save_file.store_var(self.nodes, true)
+	
+	#var array_nodes: Array[Node] = self.nodes
+	#
+	#for node in self.nodes:
+		#
+		#if node.scene_file_path.is_empty():
+			#print("persistent node '%s' is not an instanced scene, skipped" % node.name)
+			#continue
+			#
+		#if !node.has_method("save"):
+			#print("persistent node '%s' is missing a save() function, skipped" % node.name)
+			#continue
+		#
+		#var node_data = node.call("")
 
+func load_from_file() -> Variant:
+	if not FileAccess.file_exists(self.complete_path):
+		push_error("Failed to load save file at path: %s" % self.complete_path)
+		return
+	else:
+		var load_file = FileAccess.open(self.complete_path, FileAccess.READ)
+		var return_data = load_file.get_var(true)
+		return return_data
+	
 func get_save_nodes() -> Object:
 	return self.nodes
 	
@@ -38,4 +72,4 @@ func set_save_name(new_name: String) -> void:
 	self.save_name = new_name
 	
 func _to_string() -> String:
-	return "{sname}, {spath}, {snodes}".format({"sname": self.save_name, "spath": self.path, "snodes": self.nodes})
+	return "{sname}, {spath}, {snodes}, {scomplete}".format({"sname": self.save_name, "spath": self.path, "snodes": self.nodes, "complete_path": self.complete_path,})
