88a989f6f664ec5e0181638de7d7076b5cea372d
Tallon <tallon.mcabee@gmail.com>
2024-11-10 02:59:39 -0600

    Added ESC Pause Menu Key
    Can now use ESC to pause and unpause the game. Removed old pause button.

--- Scripts/Managers/ui_manager.gd
+++ Scripts/Managers/ui_manager.gd
@@ -4,7 +4,14 @@ extends Control
 
 func _ready():
 	_SettingsScreen.hide()
-
+	
+func _input(event):
+	if event.is_action_pressed("esc_pause"):
+		if (get_tree().paused):
+			_Unpause()
+		else:
+			_Pause()
+	
 func _Pause():
 	print("The Pause Button HAS BEEN PRESSED!!!")
 	get_tree().set_pause(true)
