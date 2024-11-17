7bdd3d66d08d4920f785cfbfd612a7b86ce0fdd6
Quentin M <qmorrison8@gmail.com>
2024-11-17 18:22:04 -0500

    Attempt #2 - cheat_manager + game.tscn changes

--- Scripts/Managers/cheat_manager.gd
+++ Scripts/Managers/cheat_manager.gd
@@ -5,17 +5,12 @@ extends Node
 @onready var weather: Node = $"../Player/WeatherControlNode"
 @onready var player_camera: Camera2D = $"../Player/Camera2D"
 @onready var player_sprite: AnimatedSprite2D = $"../Player/AnimatedSprite2D"
-@onready var negative_shader: Shader = preload("res://Assets/shaders/negative.gdshader")
-@onready var world_tiles: TileMapLayer = $"../TileMapLayer"
-@onready var parallax_layer01: Sprite2D = $"../ParallaxBackground/ParallaxLayer/Sprite2D"
-@onready var parallax_layer02: Sprite2D = $"../ParallaxBackground/ParallaxLayer2/Sprite2D"
-@onready var parallax_layer03: Sprite2D = $"../ParallaxBackground/ParallaxLayer3/Sprite2D"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func _on_cheat_entry_text_submitted(new_text):
+func _on_cheat_entry_text_submitted(_new_text):
 	var text: String = cheat_entry.text.strip_edges()
 	text = text.to_lower()
 	cheat_entry.clear()
@@ -51,11 +46,12 @@ func _on_cheat_entry_text_submitted(new_text):
 			player.fall_speed = 490
 		"nocheats":
 			weather.weather_select()
-			weather.rain_effect.emitting = true
-			weather.snow_effect.emitting = true
-			weather.wind_effect.emitting = true
-			var tween = get_tree().create_tween()
-			tween.parallel().tween_property(weather.sun_effect.environment, "glow_intensity", 0.15, 2)
+			if weather.rain_effect.emitting == false:
+				weather.rain_effect.emitting = true
+				weather.snow_effect.emitting = true
+				weather.wind_effect.emitting = true
+				var tween = get_tree().create_tween()
+				tween.parallel().tween_property(weather.sun_effect.environment, "glow_intensity", 0.15, 2)
 			player.max_wall_jumps = 1
 			player.coyote_time_duration = 0.125
 			player.jump_speed = -200
@@ -75,17 +71,10 @@ func _on_cheat_entry_text_submitted(new_text):
 			weather.stop_weather_sound()
 		"inverted":
 			player.inverted = true
-		"b&w":
+		"grayscale":
 			weather.sun_effect.environment.adjustment_saturation = 0.0
 		"jeb":
 			player.jeb = true
 		"invertedcamera":
 			player_camera.zoom.x *= -1
 			player_camera.zoom.y *= -1
-		"negativecat":
-			player_sprite.material.shader = negative_shader
-		"negativeworld":
-			world_tiles.material.shader = negative_shader
-			parallax_layer01.material.shader = negative_shader
-			parallax_layer02.material.shader = negative_shader
-			parallax_layer03.material.shader = negative_shader
